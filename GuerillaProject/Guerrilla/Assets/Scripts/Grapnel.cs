02a5450cb9d4dd406b56665624e1f08a4608bc4a
NWin95 <winsor.nick.cs@gmail.com>
2015-10-21 02:25:28 -0500

    Started Project
    Started the project and got a basic grapnel system working

---
+++ GuerillaProject/Guerrilla/Assets/Scripts/Grapnel.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grapnel : MonoBehaviour {
+
+    public GameObject grapnelPref;
+    public Transform grapnelTrans;
+    bool thrownBool;
+    bool swinging;
+    public LayerMask grapnelMask;
+    public Vector3 throwDirection;
+    public float throwSpeed;
+
+    void Start ()
+    {
+
+    }
+
+    void Update ()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            GrapnelThrow();
+        if (Input.GetButtonUp("Fire1") && thrownBool)
+            GrapnelDestroy();
+        if (Input.GetButtonUp("Fire1") && swinging)
+            GrapnelDetach();
+
+        if (thrownBool)
+            GrapnelCast();
+    }
+
+    void GrapnelDestroy ()
+    {
+        thrownBool = false;
+        Destroy(grapnelTrans.gameObject);
+    }
+
+    void GrapnelThrow ()
+    {
+        Vector3 pos = transform.position + (transform.forward * 1.25f);
+        GameObject grapnelObj = Instantiate(grapnelPref, pos, Quaternion.identity) as GameObject;
+        grapnelTrans = grapnelObj.GetComponent<Transform>();
+
+        grapnelTrans.GetComponent<Rigidbody>().velocity = throwDirection * throwSpeed;
+
+        thrownBool = true;
+    }
+
+    void GrapnelCast ()
+    {
+        RaycastHit hit;
+        Vector3 pos = transform.position + (transform.forward * 1.25f);
+        Vector3 gPos = grapnelTrans.position;
+        if (Physics.Linecast (pos, gPos, out hit, grapnelMask))
+        {
+            GrapnelAttach(hit);
+        }
+    }
+
+    void GrapnelDetach()
+    {
+        Destroy(gameObject.GetComponent<SpringJoint>());
+    }
+
+    void GrapnelAttach(RaycastHit hit)
+    {
+        swinging = true;
+
+        SpringJoint sj = gameObject.AddComponent<SpringJoint>();
+        sj.autoConfigureConnectedAnchor = false;
+        sj.spring = Mathf.Infinity;
+        sj.damper = Mathf.Infinity;
+
+        Vector3 pos = transform.position + (transform.forward * 1.25f);
+        float dis = Vector3.Distance(pos, hit.point);
+        sj.maxDistance = dis + 0.25f;
+
+        Rigidbody rig = hit.transform.GetComponent<Rigidbody>();
+        if (rig)
+        {
+            sj.connectedBody = rig;
+            sj.connectedAnchor = hit.transform.InverseTransformPoint(hit.point);
+        }
+        else
+        {
+            sj.connectedAnchor = hit.point;
+        }
+        GrapnelDestroy();
+
+        //StartCoroutine (JointTest(sj));
+    }
+
+    IEnumerator JointTest (SpringJoint sj)
+    {
+        yield return new WaitForSeconds(1);
+        sj.maxDistance *= 0.95f;
+        yield return new WaitForSeconds(1);
+        sj.maxDistance *= 0.95f;
+    }
+}
---
+++ GuerillaProject/Guerrilla/Assets/Scripts/Translate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Translate : MonoBehaviour {
+
+    public Vector3 moveVec;
+    public float speed;
+
+	void Start ()
+    {
+        GetComponent<Rigidbody>().velocity = moveVec * speed;
+    }
+}
